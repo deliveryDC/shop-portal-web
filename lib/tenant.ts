1cf22eb96d9ed7f057c6c4493e8adbc51b801afe
richardsuan <47155949+richardsuan@users.noreply.github.com>
2025-07-05 17:35:12 -0500

    fix sub domain

--- lib/tenant.ts
+++ lib/tenant.ts
@@ -3,9 +3,11 @@ import tienda2 from '../tenants/tienda2/config';
 import defaultConfig from '../tenants/default';
 
 export function getTenantConfig(subdomain: string) {
-  switch (subdomain) {
-    case 'PijamasReinita': return tienda1;
-    case 'EleneStore': return tienda2;
+  const sub = subdomain.toLowerCase();
+  console.log(`Subdomain: ${sub}`);
+  switch (sub) {
+    case 'pijamasreinita': return tienda1;
+    case 'elenestore': return tienda2;
     default: return defaultConfig;
   }
 }
